f756394ee2c595c547cd0cb61150ab8322147ebb
solvenbause <solvenbause@gmail.com>
2023-02-07 22:38:44 +0700

    +1

--- redux/reducers/serviceInfoReducer.ts
+++ redux/reducers/serviceInfoReducer.ts
@@ -6,12 +6,24 @@ interface IServiceInfo {
     logoService: string | null
     nameService: string | null
     expandedService: boolean | null
+    locationService: string | null
+    begindayService: string | null
+    enddayService: string | null
+    phoneService: string | null
+    webService: string | null
+    whatsappService: string | null
 }
 
 export const initialMainState: IServiceInfo = {
     logoService: null,
     nameService: null,
     expandedService: null,
+    locationService: null,
+    begindayService: null,
+    enddayService: null,
+    phoneService: null,
+    webService: null,
+    whatsappService: null,
 }
 
 
