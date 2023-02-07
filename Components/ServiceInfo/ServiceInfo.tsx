93c572ef364f3710c8357d8a6cedea1df4c4543d
Artem <artemnovikov1969@gmail.com>
2023-02-07 18:28:04 +0300

    s

---
+++ Components/ServiceInfo/ServiceInfo.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../Navigate";
+import {
+  StyleSheet,
+  TextInput,
+  Image,
+  View,
+  Text,
+  Pressable,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import FoundService from "../FoundServise/FoundService";
+
+interface ChoiseState {
+  choiseServicesReducer: {
+    textChoiseService: string;
+  };
+}
+
+type ChoiseServiceProps = {
+  navigation: StackNavigationProp<RootStackParamList>;
+};
+
+interface FoundServiceState {
+  choiseServicesReducer: {
+    dataService: [
+      {
+        nameService: string;
+        distanceToService: string;
+      }
+    ];
+  };
+}
+const ChoiseServises: React.FC<ChoiseServiceProps> = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const { textChoiseService } = useSelector(
+    (state: ChoiseState) => state.choiseServicesReducer
+  );
+  const { nameService, distanceToService } = useSelector(
+    (state: FoundServiceState) => state.choiseServicesReducer.dataService[0]
+  );
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("./../../assets/loupe.png")}
+        style={styles.image_logo}
+      ></Image>
+      <Text>{props}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#3B3B3B",
+  },
+  inner: {
+    padding: 24,
+  },
+  inputSearch: {
+    backgroundColor: "#AEAEAE",
+    borderRadius: 15,
+    width: "90%",
+    height: 40,
+    alignSelf: "center",
+    paddingLeft: 10,
+    marginTop: 20,
+    placeholderTextColor: "#ff0000",
+  },
+
+  image_logo: {
+    width: 24,
+    height: 24,
+    position: "absolute",
+    right: 30,
+    top: 29,
+  },
+});
+
+export default ChoiseServises;
--- redux/store.ts
+++ redux/store.ts
@@ -3,12 +3,12 @@ import authReducer from "./reducers/authReducer";
 import thunk from "redux-thunk";
 import choiseServicesReducer from "./reducers/choiseServicesReducer";
 import mainReducer from "./reducers/mainReducer";
-
+import serviceInfoReducer from "./reducers/serviceInfoReducer";
 let rootreducer = combineReducers({
     authReducer: authReducer,
     choiseServicesReducer: choiseServicesReducer,
     mainReducer: mainReducer,
-    
+    serviceInfoReducer: serviceInfoReducer, 
 })
 
 export type RootState = ReturnType<typeof rootreducer>;
