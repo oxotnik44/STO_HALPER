cca7a22c46c838512b01ed893823d53c886ddb71
oxotnik44 <artemnovikov1969@gmail.com>
2023-03-16 22:52:24 +0700

    e

--- Components/Authorization/Authorization.tsx
+++ Components/Authorization/Authorization.tsx
@@ -17,6 +17,7 @@ import {
   setUserPassword,
 } from "../../redux/reducers/authReducer";
 import { Dimensions } from "react-native";
+import { styles } from "./AuthorizationStyles";
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
@@ -37,38 +38,14 @@ const Authorization: React.FC<AuthorizationProps> = ({ navigation }) => {
   );
 
   const setData = () => {
-  if (login == null || password == null) {
-    Alert.alert("Внимание!", "Вы ввели неправильный логин или пароль");
-  } else {
-    const url = "https://example.com/login";
-    const data = { login, password };
-
-    fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((responseData) => {
-        // обрабатываем ответ сервера
-        console.log(responseData);
-        // вызываем действия для изменения состояния в Redux Store
-        dispatch(setUserLogin(login));
-        dispatch(setUserPassword(password));
-        navigation.navigate("ServiceInfo");
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-        Alert.alert("Ошибка!", "Произошла ошибка при выполнении запроса.");
-      });
-  }
-};
-
+    if (login == null || password == null) {
+      Alert.alert("Внимание!", "Вы ввели неправильный логин или пароль");
+    } else {
+      dispatch(setUserLogin(login));
+      dispatch(setUserPassword(password));
+      navigation.navigate("ServiceInfo");
+    }
+  };
 
   return (
     <KeyboardAvoidingView behavior="position" style={styles.container}>
@@ -106,70 +83,6 @@ const Authorization: React.FC<AuthorizationProps> = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#3B3B3B",
-  },
-  inner: {
-    padding: 24,
-  },
-  textNameCto: {
-    top: screenHeight * 0.05,
-    width: 235,
-    alignSelf: "center",
-    height: 45,
-    fontSize: 36,
-    color: "#FFFFFF",
-    marginBottom: 15,
-  },
-  imgLogo: {
-    top: screenHeight * 0.1,
-    width: screenWidth * 0.56,
-    height: screenWidth * 0.55,
-    alignSelf: "center",
-    borderBottomWidth: 1,
-  },
-  input: {
-    borderBottomWidth: 1,
-    marginBottom: 15,
-    top: screenHeight * 0.18,
-    height: screenHeight * 0.072,
-    width: screenWidth * 0.6,
-    margin: 13,
-    alignSelf: "center",
-    borderWidth: 1,
-    borderRadius: 15,
-    backgroundColor: "#AEAEAE",
-    padding: 10,
-    borderColor: "#FFD83D",
-  },
-  loginButton: {
-    width: screenWidth * 0.6,
-    height: screenHeight * 0.07,
-    alignSelf: "center",
-    backgroundColor: "#FFD83D",
-    borderRadius: 15,
-    borderBottomWidth: 1,
-    marginBottom: 25,
-    marginTop: screenHeight * 0.23,
-    justifyContent: "center",
-  },
-  textLoginButton: {
-    fontStyle: "normal",
-    fontSize: 20,
-    color: "#000000",
-    textAlign: "center",
-  },
-  registrationButton: {
-    width: screenWidth * 0.6,
-    height: screenHeight * 0.07,
-    alignSelf: "center",
-  },
-  textRegistrationButton: {
-    textAlign: "center",
-    fontSize: 24,
-  },
-});
+
 
 export default Authorization;
---
+++ Components/Authorization/AuthorizationStyles.ts
@@ -0,0 +1,73 @@
+import {
+  StyleSheet,
+} from "react-native";
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#3B3B3B",
+  },
+  inner: {
+    padding: 24,
+  },
+  textNameCto: {
+    top: screenHeight * 0.05,
+    width: 235,
+    alignSelf: "center",
+    height: 45,
+    fontSize: 36,
+    color: "#FFFFFF",
+    marginBottom: 15,
+  },
+  imgLogo: {
+    top: screenHeight * 0.1,
+    width: screenWidth * 0.56,
+    height: screenWidth * 0.55,
+    alignSelf: "center",
+    borderBottomWidth: 1,
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    top: screenHeight * 0.18,
+    height: screenHeight * 0.072,
+    width: screenWidth * 0.6,
+    margin: 13,
+    alignSelf: "center",
+    borderWidth: 1,
+    borderRadius: 15,
+    backgroundColor: "#AEAEAE",
+    padding: 10,
+    borderColor: "#FFD83D",
+  },
+  loginButton: {
+    width: screenWidth * 0.6,
+    height: screenHeight * 0.07,
+    alignSelf: "center",
+    backgroundColor: "#FFD83D",
+    borderRadius: 15,
+    borderBottomWidth: 1,
+    marginBottom: 25,
+    marginTop: screenHeight * 0.23,
+    justifyContent: "center",
+  },
+  textLoginButton: {
+    fontStyle: "normal",
+    fontSize: 20,
+    color: "#000000",
+    textAlign: "center",
+  },
+  registrationButton: {
+    width: screenWidth * 0.6,
+    height: screenHeight * 0.07,
+    alignSelf: "center",
+  },
+  textRegistrationButton: {
+    textAlign: "center",
+    fontSize: 24,
+  },
+});
+
